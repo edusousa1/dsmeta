769eb1d9ff9e251fee1002e32146ff27cad08a65
Eduardo <edusousavieira@gmail.com>
2022-07-16 10:30:33 -0300

    Heroku

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -32,6 +32,7 @@ public class SmsService {
 		
 		Sale sale = salerepository.findById(saleId).get();
 		String date = sale.getDate().getMonthValue() + "/" +sale.getDate().getYear();
+		
 		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ " 
 		+ String.format("%.2f", sale.getAmount());
 
