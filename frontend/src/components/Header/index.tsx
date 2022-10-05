c43b3e5698769819785d433b896028ff20a69e21
Eduardo Sousa <54157715+edusousa1@users.noreply.github.com>
2022-10-05 17:24:08 -0300

    Update index.tsx

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,7 +8,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://www.linkedin.com/in/eduardo-sousa-a73728197/">Eduardo</a>
                 </p>
             </div>
         </header>
